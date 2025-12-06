cc10bb4246d7b9dfd6306d408dbd7d9c83f8ae9b
Vishal-Jobin <vishaljobin.official@gmail.com>
2025-12-06 17:25:11 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,10 +1,9 @@
 // supabase-config.js
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://esm.sh/v135/@supabase/supabase-js@2.45.2/dist/module/index.js";
 
-// Your Supabase project credentials
 const SUPABASE_URL = "https://bzniniijmwrakgwjthyo.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6bmluaWlqbXdyYWtnd2p0aHlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzOTE0MjUsImV4cCI6MjA3ODk2NzQyNX0.Zn7qQ0EUixt5132FjrAhvjqQMoyB30Cl7D7LHLk8xU8";
 
-// Create the Supabase client
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
